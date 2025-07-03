8575480ac399cb379217e1e9ac6346d729d09b9f
Pooja <poojakpatel14@gmail.com>
2025-07-02 21:34:10 -0400

    Initial Commit - 1

--- Sources/BGUtility/Array+Extensions.swift
+++ Sources/BGUtility/Array+Extensions.swift
@@ -5,3 +5,10 @@
 //  Created by Pooja Patel on 2025-07-01.
 //
 
+import Foundation
+
+public extension Array {
+    subscript (safeIndex index: Int) -> Iterator.Element? {
+        return index >= 0 && index < endIndex ? self[index] : nil
+    }
+}
